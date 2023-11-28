24cddb9e45594c80f742115e27748fdefa403286
Ali <ali@ALIs-Mac-mini.local>
2023-11-28 05:17:27 +0300

    small update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,5 @@ router.use(verifyJWT);
 
 router.route("/").get(usersController.getAllUsers);
 
-// Add the route for updating flashcard progress
-router.route("/updateFlashcardProgress").post(usersController.updateFlashcardProgress);
 
 module.exports = router;
